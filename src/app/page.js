d912ecbbaf0d54aefca89cbe0aa90d14de31caa8
ChangHangeol <hgchang1@naver.com>
2024-12-14 00:44:07 +0900

    [CONFIG] 색상 추가,

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div>
-            
             <Image src="/aa_bg.webp" alt="piano" width={2000} height={400}/>
             <h1>Home</h1>
         </div>
