f1b2532f80f433a30944fa2dc7887e70ed501ed3
ChangHangeol <hgchang1@naver.com>
2025-03-29 15:04:08 +0900

    [FEAT] poster scroll -> 변수화 필요

--- src/app/fourmusics/page.js
+++ src/app/fourmusics/page.js
@@ -11,12 +11,14 @@ export default function FourMusics() {
     const [posterWidth, setPosterWidth] = useState(300);
     const [posterSrc, setPosterSrc] = useState("/fourmusics/fourmusics_poster_1.jpg");
 
+    const [posterIndex, setPosterIndex] = useState(0);
+    const posterRefs = useRef([]);
+    const posterContainerRef = useRef(null);
     const posterImageList = [
         "/fourmusics/fourmusics_poster_1.jpg",
         "/fourmusics/fourmusics_poster_2.jpg",
         "/fourmusics/fourmusics_poster_3.jpg",
     ]
-    let posterIndex = 0;
     
     const updateScrollBar = () => {
         const scrollData = getScrollProgress();
@@ -27,27 +29,33 @@ export default function FourMusics() {
         setPosterWidth(docWidth * 0.8);
     }
 
-    const changeToNextImage = (time) => {
-        posterIndex = (posterIndex + 1) % posterImageList.length;
-        setPosterSrc(posterImageList[posterIndex]);
+    useEffect(() => {
+        const leftPosterIndex = posterIndex - 1 < 0 ? posterImageList.length - 1 : posterIndex - 1;
+        const rightPosterIndex = posterIndex + 1 >= posterImageList.length ? 0 : posterIndex + 1;
+        
+        const containerX = (posterIndex-1) * -250;
+        console.log("containerX", containerX, posterWidth, posterIndex);
+        posterContainerRef.current.style.left = `${containerX}px`;
+        posterRefs.current.forEach((element, index) => {
+            if (index == posterIndex) {
+                element.classList.add(styles.poster_image_main);
+            }
+            else {
+                element.classList.remove(styles.poster_image_main);
+            }
+        });
+        setTimeout(() => changePosterIndex(), 2000);
+    }, [posterIndex]);
 
-        setTimeout(() => {
-            changeToNextImage(time);
-        }, time);
+    const changePosterIndex = () => {
+        console.log("change poster index", posterIndex, (posterIndex + 1) % posterImageList.length);
+        setPosterIndex((posterIndex + 1) % posterImageList.length);
     }
 
     useEffect(() => {
-        // const mainImage = document.querySelector(`.${styles.main_image}`);
-        // mainImage.addEventListener('animationend', () => {
-        //     mainImage.style.animation = 'none'; 
-        // });
+        setTimeout(() => changePosterIndex(), 0);   // 초기화 후 5초마다 실행.
+
         resizeEvent();
-
-
-        const iamgeChangetTime = 4000;
-        setTimeout(() => {
-            changeToNextImage(iamgeChangetTime);
-        }, iamgeChangetTime);   // 4초 뒤에 실행
 
         window.addEventListener('scroll', updateScrollBar);
         window.addEventListener('resize', updateScrollBar);
@@ -67,7 +75,23 @@ export default function FourMusics() {
             <Spacer height={20} />
             <Title2 title="Concerts" />
 
-            <Image className={styles.main_image}
+            <div 
+                className={styles.poster_container}
+                ref={posterContainerRef}
+            >
+                {
+                    posterImageList.map((src, index) => (
+                        <Image className={styles.poster_image}
+                            key={src+index}
+                            ref={(el) => posterRefs.current[index] = el} // 각 iframe에 ref 할당
+                            src={src} alt="piano" 
+                            width={0} height={0} sizes="80vw" layout="intrinsic"
+                        />
+                    ))
+                }
+            </div>
+
+            {/* <Image className={styles.main_image}
                 src={posterSrc} alt="piano" 
                 layout="intrinsic"
                 width={0}height={0} sizes="80vw"
@@ -76,7 +100,7 @@ export default function FourMusics() {
                     margin: "auto",
                     display: "block",
                 }}
-            />
+            /> */}
         </div>
     )
 }
