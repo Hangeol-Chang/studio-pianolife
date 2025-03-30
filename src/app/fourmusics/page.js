eafb48efc6fd91145c2327e5eeae6025ef5f8584
ChangHangeol <hgchang1@naver.com>
2025-03-30 17:42:06 +0900

    [STRUCT] carousel component로 분리

--- src/app/fourmusics/page.js
+++ src/app/fourmusics/page.js
@@ -8,14 +8,11 @@ import { useEffect, useRef, useState } from "react";
 import getScrollProgress from "../api/client/getScrollProgress";
 import getPageSize from "../api/client/getPageSize";
 import { YouTubeEmbed } from "@/components/media/youtube";
+import Carousel2 from "@/components/layout/carousel2";
 
 export default function FourMusics() {
     const [posterWidth, setPosterWidth] = useState(300);
-    const [posterContainerWidth, setPosterContainerWidth] = useState(1000);
 
-    const [posterIndex, setPosterIndex] = useState(0);
-    const posterRefs = useRef([]);
-    const posterContainerRef = useRef(null);
     const posterImageList = [
         "/fourmusics/fourmusics_poster_1.jpg",
         "/fourmusics/fourmusics_poster_2.jpg",
@@ -32,32 +29,9 @@ export default function FourMusics() {
 
     useEffect(() => {
         const pageSize = getPageSize();
-        setPosterContainerWidth(pageSize.width * 0.4 * posterImageList.length + 20 * (posterImageList.length - 1));
     }, [posterWidth])
 
-    useEffect(() => {        
-        const docWidth = document.documentElement.clientWidth;
-        const containerX = 0.5*getPageSize().width -(posterIndex+0.5) *posterWidth - (posterImageList.length-1) * 10;
-        console.log(containerX)
-        posterContainerRef.current.style.left = `${containerX}px`;
-        posterRefs.current.forEach((element, index) => {
-            if (index == posterIndex) {
-                element.classList.add(styles.poster_image_main);
-            }
-            else {
-                element.classList.remove(styles.poster_image_main);
-            }
-        });
-        setTimeout(() => changePosterIndex(), 5000);
-    }, [posterIndex]);
-
-    const changePosterIndex = () => {
-        setPosterIndex((posterIndex + 1) % posterImageList.length);
-    }
-
     useEffect(() => {
-        // setTimeout(() => changePosterIndex(), 0);   // 초기화 후 5초마다 실행.
-
         resizeEvent();
 
         window.addEventListener('scroll', updateScrollBar);
@@ -82,25 +56,10 @@ export default function FourMusics() {
                 언젠가 예술의 전당 무대에 올라볼 수 있는 순간을 공통 목표로 하여 모인 4명의 직장인 아마추어 피아노 앙상블
             </p>
 
-            <div 
-                className={styles.poster_container}
-                ref={posterContainerRef}
-                style={{
-                    width: `${posterContainerWidth}px`,
-                    height: `${posterWidth*2.2}px`,
-                }}
-            >
-                {
-                    posterImageList.map((src, index) => (
-                        <Image className={styles.poster_image}
-                            key={src+index}
-                            ref={(el) => posterRefs.current[index] = el} // 각 iframe에 ref 할당
-                            src={src} alt="piano" 
-                            width={posterWidth} height={0} layout="intrinsic"
-                        />
-                    ))
-                }
-            </div>
+            <Carousel2
+                imageList={posterImageList}
+                imageWidth={posterWidth}
+            />
 
             <Spacer height={20} />
             <Title2 title="Introduction"/>
