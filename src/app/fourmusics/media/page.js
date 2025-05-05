f8d7fe7d10c1ed6a9fba0f2ccd4014acf8ed60a2
ChangHangeol <hgchang1@naver.com>
2025-05-05 22:35:33 +0900

    [FEAT] fourmusics 페이지 구조 변경 진행중

---
+++ src/app/fourmusics/media/page.js
@@ -0,0 +1,14 @@
+
+import FourMusicsPlayer from '@/components/fourmusics/fourmusicsPlayer';
+import { mediaInfos } from '../media.json';
+
+export default function FourMusicsMedia() {
+
+    return (
+        <div>
+            {/* TODO : Fourmusic Player를 여기로 옮겨올 것. */}
+            포뮤직스 미디어 입니당.
+            <FourMusicsPlayer mediaInfos={mediaInfos} />
+        </div>
+    )
+}
