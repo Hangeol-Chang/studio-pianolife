c5c88fb8a4110be131e0d5605d1cef90c4265e64
ChangHangeol <hgchang1@naver.com>
2025-07-02 20:09:26 +0900

    [FEAT] Interview 페이지 메인 캐러셀 제작

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [new URL('https://img.youtube.com/**')],
+        remotePatterns: [
+            new URL('https://img.youtube.com/**'),
+            new URL('https://i.ytimg.com/**')
+        ],
     },
 }
 
 module.exports = nextConfig
-
